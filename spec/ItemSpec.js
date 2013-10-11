f1435b8dcdc569f84ba9df0eda3d18bfd97613f1
Bruno Sanches <brunopsanches@gmail.com>
2013-10-10 22:35:53 -0700

    Item can be marked as finished

--- spec/ItemSpec.js
+++ spec/ItemSpec.js
@@ -1,14 +1,21 @@
 describe("Item", function() {
   var item;
 
-  it("new item has text", function() {
+  beforeEach(function() {
     item = new Item({ text: 'New Item' });
+  });
+
+  it("new item has text", function() {
     expect(item.text).toEqual('New Item');
   });
 
   it("is created as not finished", function() {
-    item = new Item({});
     expect(item.finished).toBe(false);
+  });
+
+  it(".finish() marks item as finished", function() {
+    item.finish();
+    expect(item.finished).toBe(true);
   });
 
 });
--- src/Item.js
+++ src/Item.js
@@ -2,3 +2,7 @@ function Item(opts) {
   this.text = opts.text || '';
   this.finished = opts.finished || false;
 }
+
+Item.prototype.finish = function() {
+  this.finished = true;
+};
