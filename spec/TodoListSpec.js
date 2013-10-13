cfb205e07b4e73ae6afe61ebfa646128798820ac
Bruno Sanches <brunopsanches@gmail.com>
2013-10-12 23:46:43 -0700

    Refactor tests to use .size() and .add()

--- spec/TodoListSpec.js
+++ spec/TodoListSpec.js
@@ -16,24 +16,24 @@ describe('TodoList', function() {
 
   it(".add(item) adds new items to the collection", function() {
     todoList.add(new Item());
-    expect(todoList.list.length).toBe(1);
+    expect(todoList.size()).toBe(1);
   });
 
   it(".add(item) works with multiple items", function() {
     todoList.add(new Item());
     todoList.add(new Item());
     todoList.add(new Item());
-    expect(todoList.list.length).toBe(3);
+    expect(todoList.size()).toBe(3);
   });
 
   it(".first() returns first list element", function() {
     todoList.add(new Item({ text: 'First added item' }));
-    todoList.add(new Item());
+    todoList.add();
     expect(todoList.first().text).toMatch(/First/);
   });
 
   it(".remove(index) removes item with the specified index", function() {
-    todoList.add(new Item());
+    todoList.add();
     todoList.add(new Item({ text: 'sup, bro' }));
     todoList.remove(0);
     expect(todoList.first().text).toBe('sup, bro');
